b79c0b5381266c8f446af50e8cd034cc89199db9
itsavinnie <clown.hisokaa@gmail.com>
2024-10-16 14:22:29 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -21,35 +21,18 @@ function checkMusicState() {
     }
 }
 
-// Call the checkMusicState function when the page loads
+// Page Transition
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.body.classList.add('fade-out');
+        setTimeout(() => {
+            window.location = this.href;
+        }, 500);
+    });
+});
+
+// Check music state when the page loads
 window.onload = function() {
     checkMusicState();
 };
-
-
-// Confetti animation for the Yes page
-function confettiEffect() {
-    var end = Date.now() + (15 * 1000);
-    var colors = ['#bb0000', '#ffffff'];
-
-    (function frame() {
-        confetti({
-            particleCount: 2,
-            angle: 60,
-            spread: 55,
-            origin: { x: 0 },
-            colors: colors
-        });
-        confetti({
-            particleCount: 2,
-            angle: 120,
-            spread: 55,
-            origin: { x: 1 },
-            colors: colors
-        });
-
-        if (Date.now() < end) {
-            requestAnimationFrame(frame);
-        }
-    }());
-}
