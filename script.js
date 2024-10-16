016c6a3fab8ce7af62e464aba91ffc1e97fa403e
itsavinnie <clown.hisokaa@gmail.com>
2024-10-16 14:35:37 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ function toggleMusic() {
     var music = document.getElementById("background-music");
     if (music.paused) {
         music.play();
-        localStorage.setItem("musicPlaying", "true"); // Save play state
+        localStorage.setItem("musicPlaying", "true");
     } else {
         music.pause();
-        localStorage.setItem("musicPlaying", "false"); // Save pause state
+        localStorage.setItem("musicPlaying", "false");
     }
 }
 
-// Function to check the music state on page load
+// Check music state on load
 function checkMusicState() {
     var music = document.getElementById("background-music");
     var musicPlaying = localStorage.getItem("musicPlaying");
@@ -21,18 +21,4 @@ function checkMusicState() {
     }
 }
 
-// Page Transition
-document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.body.classList.add('fade-out');
-        setTimeout(() => {
-            window.location = this.href;
-        }, 500);
-    });
-});
-
-// Check music state when the page loads
-window.onload = function() {
-    checkMusicState();
-};
+window.onload = checkMusicState;
